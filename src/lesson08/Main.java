1320d8d4e00aadfe32804d601f65463847c347d3
nubar-rasulova <nubarrasulova0@gmail.com>
2024-06-05 17:09:13 +0400

    lesson08

---
+++ src/lesson08/Main.java
@@ -0,0 +1,20 @@
+package lesson08;
+
+// For-la islemek
+
+public class Main {
+    public static void main(String[] args) {
+//        for (int i = 0; i < 7; i++) {
+//            for (int j = 0; j < i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+        for (int i = 7; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
